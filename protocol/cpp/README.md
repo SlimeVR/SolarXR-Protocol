135c66fe22d5a081558604dc5245f120531dc17f
Ryan Butler <thebutlah@gmail.com>
2022-03-26 06:12:47 -0700

    Added platformio support, added generated files

---
+++ protocol/cpp/README.md
@@ -0,0 +1,10 @@
+# C++ protocol
+
+These files were generated with the following `flatc` command:
+
+```
+flatc --scoped-enums --cpp *.fbs
+```
+Where the * was filled in with each filename. Then the files are placed in the `include/slimevr_protocol/generated` folder.
+
+`--scoped-enums` is used because C++11 style enums are much more ergonomic :)
---
+++ protocol/cpp/include/slimevr_protocol/protocol.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#define ARDUINOSTL_M_H // Workaround to deal with <utility.h> missing in flatbuffers
+
+// Some of the #defines from platformio conflict with the protocol's enum names.
+// We fix that by temporarily undefining the conflicting #defines
+#define __TMP_ESP8266 ESP8266
+#undef ESP8266
+
+// #define ARDUINOSTL_M_H to deal with <utility.h> missing in flatbuffers
+#ifdef ARDUINOSTL_M_H
+    #define __TMP_ARDUINOSTL_M_H ARDUINOSTL_M_H
+#endif
+#define ARDUINOSTL_M_H
+
+// Actually include the generated code
+#include "generated/protocol_generated.h"
+
+// Undo the temp values and restore the originals
+#define ESP8266 __TMP_ESP8266
+#undef __TMP_ESP8266
+
+#undef ARDUINOSTL_M_H
+#ifdef __TMP_ARDUINOSTL_M_H
+    #define ARDUINOSTL_M_H __TMP_ARDUINOSTL_M_H
+    #undef __TMP_ARDUINOSTL_M_H
+#endif
