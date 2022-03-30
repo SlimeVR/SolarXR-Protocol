bcc85a265136eb912916ecd82e2cbee5df96cadb
lucas lelievre <loucass003@gmail.com>
2022-03-30 05:07:47 +0200

    typescript implementation

---
+++ protocol/typescript/src/hardware_info.ts
@@ -0,0 +1,3 @@
+export { MacAddress } from './slimevr-protocol/datatypes/mac-address';
+export { ImuId } from './slimevr-protocol/hardware-info/imu-id';
+export { McuId } from './slimevr-protocol/hardware-info/mcu-id';
---
+++ protocol/typescript/src/server.ts
@@ -0,0 +1,3 @@
+export { Acknowledgement } from './slimevr-protocol/misc/acknowledgement';
+export { InboundUnion, unionToInboundUnion, unionListToInboundUnion } from './slimevr-protocol/server/inbound-union';
+export { OutboundUnion, unionToOutboundUnion, unionListToOutboundUnion } from './slimevr-protocol/server/outbound-union';
