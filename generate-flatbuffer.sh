72fca6bead3f4eeade7f560dab143381ac9cecae
Ryan Butler <thebutlah@gmail.com>
2022-03-30 01:11:08 -0400

    Revert "Generate code script"
    This reverts commit 3ec912cf7d5662fcca59c754a827a268fc1b8333.

--- generate-flatbuffer.sh
+++
@@ -1,3 +0,0 @@
-flatc --scoped-enums -o protocol/java/src --java protocol/flatbuffers/**/*.fbs
-flatc --scoped-enums -o protocol/cpp/include/slimevr_protocol/generated --cpp protocol/flatbuffers/**/*.fbs
-flatc --scoped-enums -o protocol/typescript/src --ts protocol/flatbuffers/**/*.fbs
