a4db36e031fc33e95ab2b15bb0b1d15b3234c3e8
lucas lelievre <loucass003@gmail.com>
2022-04-01 04:39:43 +0200

    Add more tracker infos

--- generate-flatbuffer.sh
+++ generate-flatbuffer.sh
@@ -1,3 +1,3 @@
 flatc -o protocol/java/src --java protocol/flatbuffers/**/*.fbs && \
 flatc --scoped-enums -o protocol/cpp/include/slimevr_protocol/generated --cpp protocol/flatbuffers/**/*.fbs && \
-flatc -o protocol/typescript/src --ts -I .\protocol\flatbuffers\ .\protocol\flatbuffers\server.fbs .\protocol\flatbuffers\firmware.fbs
+flatc --gen-object-api -o protocol/typescript/src --ts -I .\protocol\flatbuffers\ .\protocol\flatbuffers\server.fbs .\protocol\flatbuffers\firmware.fbs
