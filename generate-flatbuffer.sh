4f87d1a5317f777f5080db1d31767491432de424
Ryan Butler <thebutlah@gmail.com>
2022-04-02 04:52:33 -0400

    Now generating rust files

--- generate-flatbuffer.sh
+++ generate-flatbuffer.sh
@@ -1,3 +1,4 @@
-flatc -o protocol/java/src --java protocol/flatbuffers/**/*.fbs && \
-flatc --scoped-enums -o protocol/cpp/include/slimevr_protocol/generated --cpp protocol/flatbuffers/**/*.fbs && \
-flatc --gen-object-api -o protocol/typescript/src --ts -I .\protocol\flatbuffers\ .\protocol\flatbuffers\server.fbs .\protocol\flatbuffers\firmware.fbs
+flatc --java -o protocol/java/src protocol/flatbuffers/**/*.fbs && \
+flatc --cpp --scoped-enums -o protocol/cpp/include/slimevr_protocol/generated protocol/flatbuffers/**/*.fbs && \
+flatc --ts --gen-object-api -o protocol/typescript/src -I ./protocol/flatbuffers/ ./protocol/flatbuffers/server.fbs ./protocol/flatbuffers/firmware.fbs && \
+flatc --rust --rust-module-root-file --gen-all -o protocol/rust/src/generated ./protocol/flatbuffers/all.fbs
