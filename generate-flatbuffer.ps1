dffb7a743377f7f71b37ad4b0fab9097dde655ae
lucas lelievre <loucass003@gmail.com>
2022-08-09 12:04:58 -0700

    Move flatbuffer folder (#35)
    * Move flatbuffer folder
    
    * add bash script

--- generate-flatbuffer.ps1
+++ generate-flatbuffer.ps1
@@ -6,7 +6,7 @@ Remove-Item -ErrorAction Ignore -Recurse  protocol/typescript/src
 Remove-Item -ErrorAction Ignore -Recurse  protocol/rust/src/generated
 
 
-./lib/flatc.exe --java --gen-object-api --gen-all -o protocol/java/src -I ./protocol/flatbuffers/ ./protocol/flatbuffers/all.fbs
-./lib/flatc.exe --cpp --scoped-enums --gen-all -o protocol/cpp/include/solarxr_protocol/generated -I ./protocol/flatbuffers/ ./protocol/flatbuffers/all.fbs
-./lib/flatc.exe --ts --gen-object-api --gen-all -o protocol/typescript/src -I ./protocol/flatbuffers/ ./protocol/flatbuffers/all.fbs
-./lib/flatc.exe --rust --rust-module-root-file --gen-all -o protocol/rust/src/generated ./protocol/flatbuffers/all.fbs
+./lib/flatc.exe --java --gen-object-api --gen-all -o protocol/java/src -I ./schema/ ./schema/all.fbs
+./lib/flatc.exe --cpp --scoped-enums --gen-all -o protocol/cpp/include/solarxr_protocol/generated -I ./schema/ ./schema/all.fbs
+./lib/flatc.exe --ts --gen-object-api --gen-all -o protocol/typescript/src -I ./schema/ ./schema/all.fbs
+./lib/flatc.exe --rust --rust-module-root-file --gen-all -o protocol/rust/src/generated ./schema/all.fbs
