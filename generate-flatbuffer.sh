15e2eb92eee34ef6a39dab5a9211064e8a384c70
lucas lelievre <loucass003@gmail.com>
2022-08-09 20:57:04 +0200

    add bash script

--- generate-flatbuffer.sh
+++ generate-flatbuffer.sh
@@ -1,5 +1,5 @@
-rm -rf protocol/java/src && flatc --java --gen-object-api --gen-all -o protocol/java/src -I ./protocol/flatbuffers/ ./protocol/flatbuffers/all.fbs && \
-rm -rf protocol/cpp/include/solarxr_protocol/generated && flatc --cpp --scoped-enums --gen-all -o protocol/cpp/include/solarxr_protocol/generated -I ./protocol/flatbuffers/ ./protocol/flatbuffers/all.fbs && \
-rm -rf protocol/typescript/src && flatc --ts --gen-object-api --gen-all -o protocol/typescript/src -I ./protocol/flatbuffers/ ./protocol/flatbuffers/all.fbs && \
-# flatc -n --gen-all -o ./test -I ./protocol/flatbuffers ./protocol/flatbuffers/all.fbs
-rm -rf protocol/rust/src/generated && flatc --rust --rust-module-root-file --gen-all -o protocol/rust/src/generated ./protocol/flatbuffers/all.fbs
+rm -rf protocol/java/src && flatc --java --gen-object-api --gen-all -o ./protocol/java/src -I ./schema/ ./schema/all.fbs && \
+rm -rf protocol/cpp/include/solarxr_protocol/generated && flatc --cpp --scoped-enums --gen-all -o ./protocol/cpp/include/solarxr_protocol/generated -I ./schema/ ./schema/all.fbs && \
+rm -rf protocol/typescript/src && flatc --ts --gen-object-api --gen-all -o ./protocol/typescript/src -I ./schema/ ./schema/all.fbs && \
+# flatc -n --gen-all -o ./test -I ./schema ./schema/all.fbs
+rm -rf protocol/rust/src/generated && flatc --rust --rust-module-root-file --gen-all -o ./protocol/rust/src/generated ./schema/all.fbs
