5486dac60963422d25f3f1462c88488149f87c12
Louka <marioluigivideo@gmail.com>
2023-01-03 01:23:07 -0500

    change BodyPart back

--- protocol/java/src/solarxr_protocol/datatypes/BodyPart.java
+++ protocol/java/src/solarxr_protocol/datatypes/BodyPart.java
@@ -21,18 +21,18 @@ public final class BodyPart {
   public static final int RIGHT_LOWER_LEG = 9;
   public static final int LEFT_FOOT = 10;
   public static final int RIGHT_FOOT = 11;
-  public static final int LEFT_SHOULDER = 12;
-  public static final int RIGHT_SHOULDER = 13;
-  public static final int LEFT_UPPER_ARM = 14;
-  public static final int RIGHT_UPPER_ARM = 15;
-  public static final int LEFT_LOWER_ARM = 16;
-  public static final int RIGHT_LOWER_ARM = 17;
+  public static final int LEFT_CONTROLLER = 12;
+  public static final int RIGHT_CONTROLLER = 13;
+  public static final int LEFT_LOWER_ARM = 14;
+  public static final int RIGHT_LOWER_ARM = 15;
+  public static final int LEFT_UPPER_ARM = 16;
+  public static final int RIGHT_UPPER_ARM = 17;
   public static final int LEFT_HAND = 18;
   public static final int RIGHT_HAND = 19;
-  public static final int LEFT_CONTROLLER = 20;
-  public static final int RIGHT_CONTROLLER = 21;
+  public static final int LEFT_SHOULDER = 20;
+  public static final int RIGHT_SHOULDER = 21;
 
-  public static final String[] names = { "NONE", "HEAD", "NECK", "CHEST", "WAIST", "HIP", "LEFT_UPPER_LEG", "RIGHT_UPPER_LEG", "LEFT_LOWER_LEG", "RIGHT_LOWER_LEG", "LEFT_FOOT", "RIGHT_FOOT", "LEFT_SHOULDER", "RIGHT_SHOULDER", "LEFT_UPPER_ARM", "RIGHT_UPPER_ARM", "LEFT_LOWER_ARM", "RIGHT_LOWER_ARM", "LEFT_HAND", "RIGHT_HAND", "LEFT_CONTROLLER", "RIGHT_CONTROLLER", };
+  public static final String[] names = { "NONE", "HEAD", "NECK", "CHEST", "WAIST", "HIP", "LEFT_UPPER_LEG", "RIGHT_UPPER_LEG", "LEFT_LOWER_LEG", "RIGHT_LOWER_LEG", "LEFT_FOOT", "RIGHT_FOOT", "LEFT_CONTROLLER", "RIGHT_CONTROLLER", "LEFT_LOWER_ARM", "RIGHT_LOWER_ARM", "LEFT_UPPER_ARM", "RIGHT_UPPER_ARM", "LEFT_HAND", "RIGHT_HAND", "LEFT_SHOULDER", "RIGHT_SHOULDER", };
 
   public static String name(int e) { return names[e]; }
 }
