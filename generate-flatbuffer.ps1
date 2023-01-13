828338935730354ffe1f0dde6291b449a1b6818a
ImUrX <urielfontan2002@gmail.com>
2023-01-12 22:21:07 -0300

    add powershell script

--- generate-flatbuffer.ps1
+++ generate-flatbuffer.ps1
@@ -6,9 +6,11 @@ Remove-Item -ErrorAction Ignore -Recurse  protocol/java/src
 Remove-Item -ErrorAction Ignore -Recurse  protocol/cpp/include/solarxr_protocol/generated
 Remove-Item -ErrorAction Ignore -Recurse  protocol/typescript/src
 Remove-Item -ErrorAction Ignore -Recurse  protocol/rust/src/generated
+Remove-Item -ErrorAction Ignore -Recurse  protocol/kotlin/lib/src
 
 
 ./flatc.exe --java --gen-object-api --gen-all -o protocol/java/src -I ./schema/ ./schema/all.fbs
 ./flatc.exe --cpp --scoped-enums --gen-all -o protocol/cpp/include/solarxr_protocol/generated -I ./schema/ ./schema/all.fbs
 ./flatc.exe --ts --gen-object-api --gen-all -o protocol/typescript/src -I ./schema/ ./schema/all.fbs
 ./flatc.exe --rust --rust-module-root-file --gen-all -o protocol/rust/src/generated ./schema/all.fbs
+./flatc --kotlin --gen-object-api --gen-all -o ./protocol/kotlin/lib/src -I ./schema/ ./schema/all.fbs
