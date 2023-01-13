e71a4049f86b9dddef29d75f416512f70297ed86
ImUrX <urielfontan2002@gmail.com>
2023-01-13 00:04:16 -0300

    dont specify kotlin version

--- protocol/kotlin/build.gradle.kts
+++ protocol/kotlin/build.gradle.kts
@@ -9,7 +9,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
-    kotlin("jvm") version "1.7.21"
+    kotlin("jvm")
 
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
