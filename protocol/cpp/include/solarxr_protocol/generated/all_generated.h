6223552f8d2017448e8b68c7b061f02300134951
unlogisch04 <98281608+unlogisch04@users.noreply.github.com>
2025-09-30 16:31:38 +0200

    correct v to capital V (#181)

--- protocol/cpp/include/solarxr_protocol/generated/all_generated.h
+++ protocol/cpp/include/solarxr_protocol/generated/all_generated.h
@@ -1047,7 +1047,7 @@ enum class BoardType : uint16_t {
   ESP32C6DEVKITC1 = 19,
   GLOVE_IMU_SLIMEVR_DEV = 20,
   GESTURES = 21,
-  SLIMEVR_v1_2 = 22,
+  SLIMEVR_V1_2 = 22,
   ESP32S3_SUPERMINI = 23,
   DEV_RESERVED = 250,
   MIN = UNKNOWN,
@@ -1078,7 +1078,7 @@ inline const BoardType (&EnumValuesBoardType())[25] {
     BoardType::ESP32C6DEVKITC1,
     BoardType::GLOVE_IMU_SLIMEVR_DEV,
     BoardType::GESTURES,
-    BoardType::SLIMEVR_v1_2,
+    BoardType::SLIMEVR_V1_2,
     BoardType::ESP32S3_SUPERMINI,
     BoardType::DEV_RESERVED
   };
@@ -1109,7 +1109,7 @@ inline const char *EnumNameBoardType(BoardType e) {
     case BoardType::ESP32C6DEVKITC1: return "ESP32C6DEVKITC1";
     case BoardType::GLOVE_IMU_SLIMEVR_DEV: return "GLOVE_IMU_SLIMEVR_DEV";
     case BoardType::GESTURES: return "GESTURES";
-    case BoardType::SLIMEVR_v1_2: return "SLIMEVR_v1_2";
+    case BoardType::SLIMEVR_V1_2: return "SLIMEVR_V1_2";
     case BoardType::ESP32S3_SUPERMINI: return "ESP32S3_SUPERMINI";
     case BoardType::DEV_RESERVED: return "DEV_RESERVED";
     default: return "";
