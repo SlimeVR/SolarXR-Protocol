2b4cba85b6bd86fca55dc334ec4eb7478782d71e
loucass003 <loucass003@gmail.com>
2025-11-18 03:24:59 +0100

    Better nix env

--- flake.nix
+++ flake.nix
@@ -4,17 +4,17 @@
   inputs.nixpkgs.url = "nixpkgs/nixos-22.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem
-    (
-      system: let
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
         pkgs = nixpkgs.legacyPackages.${system};
       in {
-        devShells.default = import ./shell.nix {inherit pkgs;};
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.flatbuffers
+            pkgs.direnv
+          ];
+        };
       }
     );
 }
